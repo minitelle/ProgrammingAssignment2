6f8619bd1e922836bca385b4ed4b2567601f3d68
minitelle <idrir1@gmail.com>
2015-05-22 00:26:44 -0400

    Revert bbaf4f2..1d68b76
    This rolls back to commit bbaf4f28806152e64cd18d60b32a7998674881ba.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##05162015 - first look at the file
+## Below are comments related to the inverted matrix and caching exercise.
+## 1. makeCacheMAtrix create an inveted matrix and caches it.
+## 2. cacheSolve references the previously cached inverted matrix and inverts it.
 
+## first crate a matrix fct
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## wrong copy paste job from the sample code for reference.
+  m <<- x$getsolve() 
+  if(!is.null(m){
+    print("here's the cached inverse of matrix x:")
+    return(m)
+  }
+  data <- x$getsolve()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
+  ##m <- NULL
+  m <- square(x)
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- mean
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## inverting the cached matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## Code to write.
 }
