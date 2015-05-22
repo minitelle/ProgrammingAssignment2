1d68b76f96bfdf0362d1d2f34b4e1d50ca98e8e4
minitelle <idrir1@gmail.com>
2015-05-22 00:21:15 -0400

    Revert "initial_testing"
    This reverts commit 3b0130aa4aa6e083d37b9ab35f466b05332b77ee.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-##05162015 - first look at the file
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
