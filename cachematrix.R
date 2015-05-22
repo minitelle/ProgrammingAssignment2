bbaf4f28806152e64cd18d60b32a7998674881ba
minitelle <idrir1@gmail.com>
2015-05-22 00:18:40 -0400

    Prelim work.
    - added comments about fcts.
    - grabbed the sample code to use or redo.
    - i.e still got work.

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,42 @@
 ## functions do
 
 ##05162015 - first look at the file
-## Write a short comment describing this function
+## Below are comments related to the inverted matrix and caching exercise.
+## 1. makeCacheMAtrix create an inveted matrix and caches it.
+## 2. cacheSolve references the previously cached inverted matrix and inverts it.
 
+## first crate a matrix fct
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## wrong copy paste job from the sample code for reference.
+  m <<- x$getsolve() 
+  if(!is.null(m){
+    print("here's the cached inverse of matrix x:")
+    return(m)
+  }
+  data <- x$getsolve()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
+  ##m <- NULL
+  m <- square(x)
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- mean
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## inverting the cached matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## Code to write.
 }
